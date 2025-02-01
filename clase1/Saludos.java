f0732ab399ae6a66d69d8e0b2902db77cd4cd7f7
Irvingcode25 <fer.irving@gmail.com>
2025-02-01 09:29:53 -0500

    Mi primer programa

---
+++ clase1/Saludos.java
@@ -0,0 +1,7 @@
+public class Saludos {
+	public static void main(String x[] ){ //metodo , entre corchete tiene que regresar un valor
+		System.out.println ("Bienvenidos a Java Programador");  //operaciones
+		System.out.println ("Sistemas UNI - 2025"); 
+	}
+
+} // todo es una clase
